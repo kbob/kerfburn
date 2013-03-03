94068afe54576b82df843a0eed53adf8a5ef9c70
Bob Miller <kbob@jogger-egg.com>
2013-03-03 12:17:49 -0800

    Started outlining what code I need to write.

--- back/main.c
+++ back/main.c
@@ -1,5 +1,21 @@
+#include <stdbool.h>
+
+void initialize_devices()
+{
+    // Start the millisecond timer.
+    // Initialize the serial line.
+    // (That's enough for now.)
+}
+
+void do_housekeeping()
+{
+    // If the status packet flag is set, generate status packet
+    // If we've received a full command packet, enqueue the step generator.
+}
+
 int main()
 {
-    // init things
-    // what is the background process?
+    initialize_devices();
+    while (true)
+        do_housekeeping();
 }
