4f8addb3f8182758b17c2bd4c721effb02abd6de
Bob Miller <kbob@jogger-egg.com>
2013-03-16 20:05:57 -0700

    Skeleton of emergency stop mechanism.

---
+++ back/e-stop.c
@@ -0,0 +1,13 @@
+#include "e-stop.h"
+
+#include "fw_assert.h"
+
+void emergency_stop_NONATOMIC(void)
+{
+    fw_assert(0);
+}
+
+void emergency_stop(void)
+{
+    fw_assert(0);
+}
---
+++ back/e-stop.h
@@ -0,0 +1,7 @@
+#ifndef E_STOP_included
+#define E_STOP_included
+
+extern void emergency_stop_NONATOMIC(void); // XXX should be inline.
+extern void emergency_stop(void);
+
+#endif /* !E_STOP_included */
