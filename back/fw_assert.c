0c07e5e0118c60c1a2bb0f22fb69eeb43f73214f
Bob Miller <kbob@jogger-egg.com>
2013-03-16 19:49:40 -0700

    Improved assert repeatedly prints the source line number that failed.

--- back/fw_assert.c
+++ back/fw_assert.c
@@ -1,10 +1,104 @@
 #include "fw_assert.h"
 
-#include <stdlib.h>
+// Firmware Assert.
+//
+// Loop forever:
+//    print "Assertion failed on line %d.\r\n"
+//    throb LED once.
+//
+// Interrupts are permanently off.
+// Use local serial and LED drivers -- this will keep working when
+// other drivers are broken.  (This will also break when other parts
+// are working.
+//
+// If FW_NDEBUG is defined, this whole file goes away.
 
-__attribute__((noreturn))
-extern void fw_assertion_failed(int line_no)
+#ifndef FW_NDEBUG
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#define __DELAY_BACKWARD_COMPATIBLE__
+#include <util/delay.h>
+
+static inline void init_serial(void)
 {
-    abort();
+    // Keep mode, baud rate, parity settings.
+    UCSR0B &= ~_BV(RXEN0);
+    UCSR0B &= ~_BV(TXEN0);
+    UCSR0B |=  _BV(TXEN0);;
 }
 
+static inline void put_char(uint8_t c)
+{
+    while (bit_is_clear(UCSR0A, UDRE0))
+        continue;
+    UDR0 = c;
+}
+
+static inline void put_str(char *p)
+{
+    while (*p)
+        put_char(*p++);
+}
+
+static inline void put_dec(unsigned int n)
+{
+    char b[5];
+    uint8_t i = 0;
+    do {
+        b[i++] = '0' + n % 10;
+    } while (n /= 10);
+    while (i)
+        put_char(b[--i]);
+}
+
+static inline void init_LED(void)
+{
+    DDRB  |= _BV(DDB7);
+}
+
+static inline void light_LED(void)
+{
+    PORTB |= _BV(PB7);
+}
+
+static inline void extinguish_LED(void)
+{
+    PORTB &= ~_BV(PB7);
+}
+
+static void throb_once(void)
+{
+    int duty;
+    for (duty = 0; duty < 30; duty++) {
+        light_LED();
+        _delay_ms(duty);
+        extinguish_LED();
+        _delay_ms(30 - duty);
+    }
+    for (duty = 30; duty > 0; --duty) {
+        light_LED();
+        _delay_ms(duty);
+        extinguish_LED();
+        _delay_ms(30 - duty);
+    }
+}
+
+__attribute__((noreturn))
+extern void fw_assertion_failed(unsigned int line_no)
+{
+    cli();
+    init_serial();
+    init_LED();
+    while (true) {
+        put_str("Assertion failed on line ");
+        put_dec(line_no);
+        put_str(".\r\n");
+        throb_once();
+    }
+}
+
+#endif
