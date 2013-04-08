e8962a6afd6a5f231fab024d8338552911cc7c18
Bob Miller <kbob@jogger-egg.com>
2013-04-08 02:19:18 -0700

    Use MSG_NOSIGNAL when sending to receivers to prevent SIGPIPE.

--- front/thruport/receiver_service.c
+++ front/thruport/receiver_service.c
@@ -55,7 +55,7 @@ void instantiate_receiver_service(int sock)
 static int send_to_receiver(receiver *r, const char *data, size_t count)
 {
     while (count) {
-        ssize_t nw = send(r->r_fd, data, count, MSG_DONTWAIT);
+        ssize_t nw = send(r->r_fd, data, count, MSG_DONTWAIT | MSG_NOSIGNAL);
         if (nw < 0) {
             syslog(LOG_WARNING, "receiver send failed: %m");
             return nw;
