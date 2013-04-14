012272951a8dc72e5bdd63d84a2ee4d3644b15ac
Bob Miller <kbob@jogger-egg.com>
2013-04-13 20:22:20 -0700

    stderr now writes synchronously, blocks interrupts, bypasses serial driver. Can write error messages from interrupt level.

--- back/fw_stdio.c
+++ back/fw_stdio.c
@@ -2,27 +2,44 @@
 
 #include <stdio.h>
 
+#include <avr/io.h>
+#include <util/atomic.h>
+
+#include "fw_assert.h"
 #include "serial.h"
 
-static int my_putc(char c, FILE *stream)
+static int putc_stout(char c, FILE *stream)
 {
-    if (c == '\n') {
-        while (!serial_tx_is_available())
-            continue;
-        serial_tx_put_char('\r');
-    }
+    if (c == '\n')
+        putc_stout('\r', stream);
     while (!serial_tx_is_available())
         continue;
-    serial_tx_put_char(c);
+    bool ok = serial_tx_put_char(c);
+    fw_assert(ok);
     if (c == '\n')
         while (!serial_tx_is_idle())
             continue;
     return 0;
 }
 
-static FILE my_stdouterr = FDEV_SETUP_STREAM(my_putc, NULL, _FDEV_SETUP_WRITE);
+static int putc_sterr(char c, FILE *stream)
+{
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if (c == '\n') {
+            loop_until_bit_is_set(UCSR0A, UDRE0);
+            UDR0 = '\r';
+        }
+        loop_until_bit_is_set(UCSR0A, UDRE0);
+        UDR0 = c;
+    }
+    return 0;
+}
+
+static FILE fw_stout = FDEV_SETUP_STREAM(putc_stout, NULL, _FDEV_SETUP_WRITE);
+static FILE fw_sterr = FDEV_SETUP_STREAM(putc_sterr, NULL, _FDEV_SETUP_WRITE);
 
 void init_stdio(void)
 {
-    stdout = stderr = &my_stdouterr;
+    stdout = &fw_stout;
+    stderr = &fw_sterr;
 }
