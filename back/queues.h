0ad8981bee74fec9bb88c4a294ab58d80f4113a4
Bob Miller <kbob@jogger-egg.com>
2013-04-22 00:51:00 -0700

    Made enqueue_atom_X() atomic.  Added undequeue_atom_X().

--- back/queues.h
+++ back/queues.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include <util/atomic.h>
+
 #include "atoms.h"
 
 #include "fw_assert.h"
@@ -16,27 +18,27 @@ typedef struct queue_private queue;
 // Queues for X, Y, Z motors and laser pulses.
 extern queue Xq, Yq, Zq, Pq;
 
-static inline void init_queues             (void);
+static inline void     init_queues              (void);
 
-static inline bool queue_is_running        (const queue *);
-static inline bool queue_is_empty          (const queue *);
-static inline bool queue_is_full           (const queue *);
+static inline bool     queue_is_running         (const queue *);
+static inline bool     queue_is_empty           (const queue *);
+static inline bool     queue_is_full            (const queue *);
 
-extern        void start_queues            (void);
-extern        void await_queues_empty      (void);
-extern        void stop_queues_immediately (void);
+extern        void     start_queues             (void);
+extern        void     await_queues_empty       (void);
+extern        void     stop_queues_immediately  (void);
 
-static inline void enqueue_atom_X          (atom);
-static inline atom dequeue_atom_X          (void);
+static inline void     enqueue_atom_X           (uint16_t);
+static inline uint16_t dequeue_atom_X_NONATOMIC (void);
 
-static inline void enqueue_atom_Y          (atom);
-static inline atom dequeue_atom_Y          (void);
+static inline void     enqueue_atom_Y           (uint16_t);
+static inline uint16_t dequeue_atom_Y_NONATOMIC (void);
 
-static inline void enqueue_atom_Z          (atom);
-static inline atom dequeue_atom_Z          (void);
+static inline void     enqueue_atom_Z           (uint16_t);
+static inline uint16_t dequeue_atom_Z_NONATOMIC (void);
 
-static inline void enqueue_atom_P          (atom);
-static inline atom dequeue_atom_P          (void);
+static inline void     enqueue_atom_P           (uint16_t);
+static inline uint16_t dequeue_atom_P_NONATOMIC (void);
 
 
 // Implementation
@@ -71,35 +73,55 @@ static inline bool queue_is_full(const queue *q)
     return q->q_head == (uint8_t)(q->q_tail + 2);
 }
 
-#define DEFINE_ENQUEUE_DEQUEUE(Q)               \
-                                                \
-    static inline void enqueue_atom_##Q(atom a) \
-    {                                           \
-        union {                                 \
-            uint8_t b[2];                       \
-            int    *p;                          \
-        } u;                                    \
-                                                \
-        fw_assert(!queue_is_full(&Q##q));       \
-        u.b[0] = Q##q.q_tail;                   \
-        u.b[1] = (uintptr_t)Q##q_buf >> 8;      \
-        *u.p++ = a;                             \
-        Q##q.q_tail = u.b[0];                   \
-    }                                           \
-                                                \
-    static inline atom dequeue_atom_##Q(void)   \
-    {                                           \
-        union {                                 \
-            uint8_t b[2];                       \
-            int    *p;                          \
-        } u;                                    \
-                                                \
-        fw_assert(!queue_is_empty(&Q##q));      \
-        u.b[0] = Q##q.q_head;                   \
-        u.b[1] = (uintptr_t)Q##q_buf >> 8;      \
-        atom a = *u.p++;                        \
-        Q##q.q_head = u.b[0];                   \
-        return a;                               \
+#define DEFINE_ENQUEUE_DEQUEUE(Q)                                       \
+                                                                        \
+    static inline void enqueue_atom_##Q(uint16_t a)                     \
+    {                                                                   \
+        union {                                                         \
+            uint8_t   b[2];                                             \
+            uint16_t *p;                                                \
+        } u;                                                            \
+                                                                        \
+        print_atom("enqueue " #Q, a);                                   \
+        u.b[1] = (uintptr_t)Q##q_buf >> 8;                              \
+        while (queue_is_full(&Q##q))                                    \
+            continue;                                                   \
+        ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {                             \
+            u.b[0] = Q##q.q_tail;                                       \
+            *u.p++ = a;                                                 \
+            Q##q.q_tail = u.b[0];                                       \
+        }                                                               \
+    }                                                                   \
+                                                                        \
+    static inline uint16_t dequeue_atom_##Q##_NONATOMIC(void)           \
+    {                                                                   \
+        union {                                                         \
+            uint8_t   b[2];                                             \
+            uint16_t *p;                                                \
+        } u;                                                            \
+                                                                        \
+        fw_assert(!queue_is_empty(&Q##q));                              \
+        u.b[0] = Q##q.q_head;                                           \
+        u.b[1] = (uintptr_t)Q##q_buf >> 8;                              \
+        uint16_t a = *u.p++;                                            \
+        Q##q.q_head = u.b[0];                                           \
+        print_atom("dequeue " #Q, a);                                   \
+        return a;                                                       \
+    }                                                                   \
+                                                                        \
+    static inline void undequeue_atom_##Q##_NONATOMIC(uint16_t a)       \
+    {                                                                   \
+        union {                                                         \
+            uint8_t   b[2];                                             \
+            uint16_t *p;                                                \
+        } u;                                                            \
+                                                                        \
+        fw_assert(!queue_is_full(&Q##q));                               \
+        print_atom("undequeue " #Q, a);                                 \
+        u.b[0] = Q##q.q_head - 2;                                       \
+        u.b[1] = (uintptr_t)Q##q_buf >> 8;                              \
+        *u.p = a;                                                       \
+        Q##q.q_head = u.b[0];                                           \
     }
 
 DEFINE_ENQUEUE_DEQUEUE(X);
