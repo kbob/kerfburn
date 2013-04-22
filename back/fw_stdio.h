e0a6c0c09b76776b2334dc63e04987d473d5ab6c
Bob Miller <kbob@jogger-egg.com>
2013-04-21 19:46:37 -0700

    Defined PSL macro: Program space String Literal.

--- back/fw_stdio.h
+++ back/fw_stdio.h
@@ -1,6 +1,11 @@
 #ifndef FW_STDIO_included
 #define FW_STDIO_included
 
+#include <avr/pgmspace.h>
+
+// PSL: program space string literal
+#define PSL(s) ({ static const char x[] PROGMEM = (s); x; })
+
 extern void init_stdio(void);
 
 #endif /* !FW_STDIO_included */
