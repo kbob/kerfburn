af59f04b9ff699f18fea0af9381415b1f2f58200
Bob Miller <kbob@jogger-egg.com>
2013-04-28 21:11:54 -0700

    stop_x_timer() now uses the right register.  Added set_[xyz]_step_interval().

--- back/motors.h
+++ back/motors.h
@@ -42,13 +42,18 @@ static inline void disable_x_step(void)
 
 static inline void stop_x_timer(void)
 {
-    X_MOTOR_STEP_TCCRA &= ~_BV(X_MOTOR_STEP_CS0);
+    X_MOTOR_STEP_TCCRB &= ~_BV(X_MOTOR_STEP_CS0);
 }
 
 static inline void clear_x_step(void)
 {
-    X_MOTOR_STEP_TCCRA &= ~_BV(X_MOTOR_STEP_COM1);
     SET_REG_BIT(X_MOTOR_STEP_PORT, X_MOTOR_STEP_OFF);
+    X_MOTOR_STEP_TCCRA &= ~_BV(X_MOTOR_STEP_COM1);
+}
+
+static inline void set_x_step_interval(uint16_t ticks)
+{
+    X_MOTOR_STEP_ICR = ticks;
 }
 
 
@@ -74,10 +79,30 @@ static inline void set_y_direction_negative(void)
     SET_REG_BIT(Y_MOTOR_DIRECTION_PORT, Y_MOTOR_DIRECTION_NEGATIVE);
 }
 
-static inline void clear_y_step(void)
+static inline void enable_y_step(void)
+{
+    Y_MOTOR_STEP_TCCRA |= _BV(Y_MOTOR_STEP_COM1);
+}
+
+static inline void disable_y_step(void)
 {
     Y_MOTOR_STEP_TCCRA &= ~_BV(Y_MOTOR_STEP_COM1);
+}
+
+static inline void stop_y_timer(void)
+{
+    Y_MOTOR_STEP_TCCRB &= ~_BV(Y_MOTOR_STEP_CS0);
+}
+
+static inline void clear_y_step(void)
+{
     SET_REG_BIT(Y_MOTOR_STEP_PORT, Y_MOTOR_STEP_OFF);
+    Y_MOTOR_STEP_TCCRA &= ~_BV(Y_MOTOR_STEP_COM1);
+}
+
+static inline void set_y_step_interval(uint16_t ticks)
+{
+    Y_MOTOR_STEP_ICR = ticks;
 }
 
 
@@ -103,10 +128,29 @@ static inline void set_z_direction_negative(void)
     SET_REG_BIT(Z_MOTOR_DIRECTION_PORT, Z_MOTOR_DIRECTION_NEGATIVE);
 }
 
-static inline void clear_z_step(void)
+static inline void enable_z_step(void)
 {
-    Z_MOTOR_STEP_TCCRA &= ~_BV(Z_MOTOR_STEP_COM1);
-    SET_REG_BIT(Z_MOTOR_STEP_PORT, Z_MOTOR_STEP_OFF);
+    Z_MOTOR_STEP_TCCRA |= _BV(Z_MOTOR_STEP_COM1);
 }
 
+static inline void disable_z_step(void)
+{
+    Z_MOTOR_STEP_TCCRA &= ~_BV(Z_MOTOR_STEP_COM1);
+}
+
+static inline void stop_z_timer(void)
+{
+    Z_MOTOR_STEP_TCCRB &= ~_BV(Z_MOTOR_STEP_CS0);
+}
+
+static inline void clear_z_step(void)
+{
+    SET_REG_BIT(Z_MOTOR_STEP_PORT, Z_MOTOR_STEP_OFF);
+    Z_MOTOR_STEP_TCCRA &= ~_BV(Z_MOTOR_STEP_COM1);
+}
+
+static inline void set_z_step_interval(uint16_t ticks)
+{
+    Z_MOTOR_STEP_ICR = ticks;
+}
 #endif /* !MOTORS_included */
