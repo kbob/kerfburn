f3a1857c3406c0f2e912a44da5cea547e3d880f0
Bob Miller <kbob@jogger-egg.com>
2013-05-08 09:41:59 -0700

    init_lasers() is no longer inline.

--- back/lasers.c
+++ back/lasers.c
@@ -1,1 +1,20 @@
 #include "lasers.h"
+
+#include <avr/interrupt.h>
+
+#include "fw_assert.h"
+
+void init_lasers(void)
+{
+    // Initialize pins.
+    INIT_OUTPUT_PIN(MAIN_LASER_PULSE,    MAIN_LASER_PULSE_OFF);
+    INIT_OUTPUT_PIN(VISIBLE_LASER_PULSE, VISIBLE_LASER_PULSE_OFF);
+
+    // Initialize timer.
+    LASER_PULSE_TCCRA       = _BV(LASER_PULSE_WGM1);
+    LASER_PULSE_TCCRB       = _BV(LASER_PULSE_WGM3) | _BV(LASER_PULSE_WGM2);
+    MAIN_LASER_PULSE_OCR    = 1;
+    VISIBLE_LASER_PULSE_OCR = 1;
+    LASER_PULSE_TIMSK       = _BV(LASER_PULSE_TOIE);
+    LASER_PULSE_TIFR        = _BV(LASER_PULSE_TOV);
+}
--- back/lasers.h
+++ back/lasers.h
@@ -1,23 +1,11 @@
 #ifndef LASERS_included
+#define LASERS_included
 
 #include "config/pin-defs.h"
 
 #include "pin-io.h"
 
-static inline void init_lasers(void)
-{
-    // Initialize pins.
-    INIT_OUTPUT_PIN(MAIN_LASER_PULSE,    MAIN_LASER_PULSE_OFF);
-    INIT_OUTPUT_PIN(VISIBLE_LASER_PULSE, VISIBLE_LASER_PULSE_OFF);
-
-    // Initialize timer.
-    LASER_PULSE_TCCRA       = _BV(LASER_PULSE_WGM1);
-    LASER_PULSE_TCCRB       = _BV(LASER_PULSE_WGM3) | _BV(LASER_PULSE_WGM2);
-    MAIN_LASER_PULSE_OCR    = 1;
-    VISIBLE_LASER_PULSE_OCR = 1;
-    LASER_PULSE_TIMSK       = _BV(LASER_PULSE_TOIE);
-    LASER_PULSE_TIFR        = _BV(LASER_PULSE_TOV);
-}
+extern void init_lasers(void);
 
 static inline void set_laser_pulse_interval(uint16_t interval)
 {
