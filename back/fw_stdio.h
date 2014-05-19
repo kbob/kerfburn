e9950d2cda9c96d5b6902b52d7ba5738f31f6c0b
kbob <kbob@jogger-egg.com>
2014-05-18 18:44:52 -0700

    Better compiler error when printf is referenced.

--- back/fw_stdio.h
+++ back/fw_stdio.h
@@ -6,14 +6,14 @@
 extern void init_stdio(void);
 
 #ifndef PRINTF_COMPAT
-  #define printf    is deprecated, please use    printf_P
-  #define fprintf   is deprecated, please use   fprintf_P
-  #define sprintf   is deprecated, please use   sprintf_P
-  #define snprintf  is deprecated, please use  snprintf_P
+  #define printf    printf_is_deprecated, please use    printf_P
+  #define fprintf   fprintf_is_deprecated, please use   fprintf_P
+  #define sprintf   sprintf_is_deprecated, please use   sprintf_P
+  #define snprintf  snprintf_is_deprecated, please use  snprintf_P
 // AVR-libc is missing vprintf_P.
-  #define vfprintf  is deprecated, please use  vfprintf_P
-  #define vsprintf  is deprecated, please use  vsprintf_P
-  #define vsnprintf is deprecated, please use vsnprintf_P
+  #define vfprintf  vfprintf_is_deprecated, please use  vfprintf_P
+  #define vsprintf  vsprintf_is_deprecated, please use  vsprintf_P
+  #define vsnprintf vsnprintf_is_deprecated, please use vsnprintf_P
 #endif
 
 #endif /* !FW_STDIO_included */
